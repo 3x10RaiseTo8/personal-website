e955e7ca1d0ab2a9da9b95d8f585104256d4a069
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-03-21 16:52:50 +0530

    prettier-tailwind working but shadcn not working

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Abhishek Singh';
-export const SITE_DESCRIPTION = 'Portfolio/Blog';
+export const SITE_TITLE = "now7x";
+export const SITE_DESCRIPTION = "Portfolio/Blog";
