25bb1c63f0e0694e8da34820b2c9a4be7ca6b28b
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-03-16 19:50:12 +0530

    change the test-private location to root dir

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders"; // Not available with legacy API
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/content/test-private" }),
+    loader: glob({ pattern: "**/*.md", base: "./test-private" }),
 });
 
 export const collections = { blog };
