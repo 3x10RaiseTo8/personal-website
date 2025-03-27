9fce3b2521c790a07ccf3ef20e796d94ca5c2757
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-03-28 00:54:52 +0530

    some, no, a lot of changes
    Signed-off-by: Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+    title: "abhishek.",
+    description: "brain made thoughts",
+    author: "Abhishek Singh",
+    website: "https://abhishe.com", // replace this with your deployed domain
+    profile: "https://abhishe.com",
+    ogImage: "og.jpg",
+} as const;
