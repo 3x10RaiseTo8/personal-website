0317a4f3d192822602bfd34db1dc139f07098ed6
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-08-06 20:52:10 +0530

    caps title

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
-    title: "abhishek.",
-    description: "brain made thoughts",
-    author: "Abhishek Singh",
-    website: "https://abhishe.com", // replace this with your deployed domain
-    profile: "https://abhishe.com",
-    ogImage: "og.jpg",
+  title: "Abhishek",
+  description: "brain made thoughts",
+  author: "Abhishek Singh",
+  website: "https://abhishe.com", // replace this with your deployed domain
+  profile: "https://abhishe.com",
+  ogImage: "og.jpg",
 } as const;
