fcf9f5ef91085d13a87e3772daf41abeec49b41e
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-08-20 10:27:40 +0530

    scary bulk changes

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,17 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders"; // Not available with legacy API
 
-const blog = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./content/posts/" }),
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./content/posts/" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    draft: z.boolean(),
+    layout: z.string().optional(),
+    publishDate: z.coerce.date(),
+    lastModified: z.coerce.date().optional(),
+    slug: z.string(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { posts };
