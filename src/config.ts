7b5657e718893f0765ec7fd07cb5499d07c5c5b8
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-09-07 00:19:37 +0530

    Merge Head in Layout

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,21 @@
-export const SITE = {
+export interface SiteConfig {
+  title: string;
+  description: string;
+  author: string;
+  website: string;
+  ogImage: string;
+  dir: "ltr" | "rtl" | "auto";
+  lang: string;
+  timezone: string;
+}
+
+export const SITE: SiteConfig = {
   title: "Abhishek",
   description: "brain made thoughts",
   author: "Abhishek Singh",
   website: "https://abhishe.com", // replace this with your deployed domain
-  profile: "https://abhishe.com",
   ogImage: "og.jpg",
+  dir: "ltr",
+  lang: "en",
+  timezone: "Asia/Kolkata",
 } as const;
