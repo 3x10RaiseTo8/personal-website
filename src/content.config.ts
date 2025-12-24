6b7d380d173b7b1e5a3a4f1cede714cef7400924
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-12-24 13:17:41 +0530

    Take one

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,16 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders"; // Not available with legacy API
 
-const posts = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./content/posts/" }),
+const essays = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./content/essays/" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     draft: z.boolean(),
-    layout: z.string().optional(),
+    slug: z.string(),
     publishDate: z.coerce.date(),
     lastModified: z.coerce.date().optional(),
-    slug: z.string(),
   }),
 });
 
-export const collections = { posts };
+export const collections = { essays };
