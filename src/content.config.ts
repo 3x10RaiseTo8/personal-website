10b8ecdc926c198470b57129ca2816c2c44d349b
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2026-01-07 08:17:11 +0530

    all working

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,32 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders"; // Not available with legacy API
 
-const essays = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./content/essays/" }),
+const posts = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./content/posts/" }),
   schema: z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().optional().nullable(),
     draft: z.boolean(),
     slug: z.string(),
     publishDate: z.coerce.date(),
     lastModified: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+    category: z
+      .enum(["resources", "essays", "uncategorized"])
+      .default("uncategorized"),
   }),
 });
 
-export const collections = { essays };
+const pages = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./content/pages/" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional().nullable(),
+    draft: z.boolean().optional().default(true),
+    slug: z.string(),
+    publishDate: z.coerce.date().optional(),
+    lastModified: z.coerce.date().optional(),
+  }),
+});
+
+export const collections = { posts, pages };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,19 @@
 import rss from "@astrojs/rss";
-import { SITE } from "../config";
+import { SITE } from "../siteConfig";
 
 export function GET(context: { site: string }) {
-    return rss({
-        title: "abhishe blog",
-        description: SITE.description,
+  return rss({
+    title: "abhishe blog",
+    description: SITE.description,
 
-        // Pull in your project "site" from the endpoint context
-        // https://docs.astro.build/en/reference/api-reference/#site
-        site: context.site,
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#site
+    site: context.site,
 
-        // Array of `<item>`s in output xml
-        items: [],
+    // Array of `<item>`s in output xml
+    items: [],
 
-        // (optional) inject custom xml
-        customData: `<language>en-us</language>`,
-    });
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
 }
