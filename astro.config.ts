8d8c1c9cb508d34672a35aa519a4a98eca9eed63
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2026-01-10 17:22:41 +0530

    add linkedin x github redirects

--- astro.config.ts
+++ astro.config.ts
@@ -20,5 +20,9 @@ export default defineConfig({
   redirects: {
     "/talent":
       "https://docs.google.com/forms/d/e/1FAIpQLScYNoiJFctAxkgqDF564bJ_dij1HM4269V8S-9WcNla7PQVzA/viewform?usp=dialog",
+    "/linkedin": "https://www.linkedin.com/in/abhishek7x/",
+    "/twitter": "https://x.com/now7x",
+    "/x": "https://x.com/now7x",
+    "/github": "https://github.com/3x10RaiseTo8",
   },
 });
