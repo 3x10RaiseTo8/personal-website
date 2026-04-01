f180da49c27bd8357c6a148f348ec3ad7ad07b4e
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2026-04-01 17:34:05 +0530

    remove rss styles

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -19,15 +19,15 @@ export async function GET(context: { site: string }) {
     // Array of `<item>`s in output xml
     items: sortedPublishedPosts.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.publishDate,
       description: post.data.description,
       link: `/p/${post.id}`,
-      categories: post.data.tags,
+      pubDate: post.data.publishDate,
+      categories: [post.data.category, ...(post.data.tags || [])],
       author: SITE.author,
     })),
 
     // (optional) inject custom xml
     customData: `<generator>https://astro.build</generator><webMaster>${SITE.links[0].text}</webMaster><image>${new URL(SITE.defaultOgImage, context.site)}</image><ttl>10</ttl><docs>https://www.rssboard.org/rss-specification</docs><language>${SITE.lang}</language><copyright>${SITE.copyrightText}</copyright><lastBuildDate>${new Date().toUTCString()}</lastBuildDate>`,
-    stylesheet: "/rss/pretty-feed-v3.xsl",
+    // stylesheet: "/rss/pretty-feed-v3.xsl",
   });
 }
